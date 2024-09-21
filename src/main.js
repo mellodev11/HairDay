ce5c77c80ba4b2052523767e909216c89f662400
mellodev11 <robertale45@gmail.com>
2024-09-21 10:01:33 -0300

    finalizando a instação de pacotes server

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import "./styles/global.css";
 
 import "./styles/form.css";
 import "./styles/schedule.css";
+
+import "./libs/dayjs.js";
+
+import dayjs from "dayjs";
+console.log(dayjs().format("HH:mm"));
